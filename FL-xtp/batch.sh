f90e5bf1b7bacf380a3784e7ba3a3bbc9ee04846
tom <desa2thomas@gmail.com>
2025-12-11 16:05:09 -0500

    removed batch job script

--- FL-xtp/batch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=01-00:30
-#SBATCH --gpus=h100
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=32gb
-
-export WANDB_API_KEY="efe870a3e39f7ac1dde74d2cfedd1fbb0287cbce"
-. setup.sh
-flwr run ./
